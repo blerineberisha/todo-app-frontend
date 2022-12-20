1f6c520403e16fc286b9e8c526f7b623b305a286
Shawn Lacarta <shawnlacarta@Shawns-MacBook-Pro.local>
2022-12-20 10:31:44 +0100

    removed unused imports

--- todoapp-fe/src/App.tsx
+++ todoapp-fe/src/App.tsx
@@ -1,7 +1,6 @@
 import { Typography } from "@mui/material";
 import React from "react";
 import "./App.css";
-import TaskCreationDialog from "./UI/organisms/TaskCreationDialog";
 
 function App() {
   return (
