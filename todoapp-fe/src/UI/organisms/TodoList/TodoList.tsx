7f156e9ddfc805682129999a77e55d3ae485d02d
data-corrupted <leandro@piras.li>
2023-01-15 12:32:56 +0100

    Fixed formatting to match mockup

--- todoapp-fe/src/UI/organisms/TodoList/TodoList.tsx
+++ todoapp-fe/src/UI/organisms/TodoList/TodoList.tsx
@@ -1,13 +1,24 @@
+import "./TodoList.scss";
 import Task, { TaskProps } from "../../molecules/Task/Task";
+import TaskCreationDialog from "../TaskCreationDialog";
 
 interface TodoListProps {
+  name: string;
   tasks: TaskProps[];
 }
 
 function TodoList(props: TodoListProps) {
-  const tasks = props.tasks.map((element) => <Task {...element} />);
+  const tasks = props.tasks.map((prop) => <Task {...prop} />);
 
-  return <div className="TodoList">{tasks}</div>;
+  return (
+    <div className="TodoList">
+      <div className="TodoList-header">
+        <h1>{props.name}</h1>
+        <TaskCreationDialog />
+      </div>
+      <div className="TodoList-tasks">{tasks}</div>
+    </div>
+  );
 }
 
 export default TodoList;
