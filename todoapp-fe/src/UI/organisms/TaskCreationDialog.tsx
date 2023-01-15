74c0f9e0e8d2e32a66d8b87b961e0e558e860c03
data-corrupted <leandro@piras.li>
2023-01-15 12:04:57 +0100

    Wrapped in ThemeProvider and removed sx

--- todoapp-fe/src/UI/organisms/TaskCreationDialog.tsx
+++ todoapp-fe/src/UI/organisms/TaskCreationDialog.tsx
@@ -5,11 +5,12 @@ import Dialog from "@mui/material/Dialog";
 import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
 import { useState } from "react";
-import { DialogTitle } from "@mui/material";
+import { DialogTitle, ThemeProvider } from "@mui/material";
 import "../styles/TaskCreationDialog.scss";
 import { Form, Formik, FormikProps } from "formik";
 import * as yup from "yup";
 import { TaskCreation } from "../../models/TaskCreationDialog.model";
+import theme from "../Theme";
 
 const TaskCreationDialog = () => {
   const [open, setOpen] = useState(false);
@@ -49,77 +50,71 @@ const TaskCreationDialog = () => {
           isSubmitting,
         } = props;
         return (
-          <Form>
-            <Button variant="contained" onClick={handleClickOpen} sx={{ backgroundColor: "#49B093" }}>
-              Add task
-            </Button>
-            <Dialog open={open} onClose={handleClose} fullWidth maxWidth="sm">
-              <DialogTitle className="dialog_title">Add a new task</DialogTitle>
-              <DialogContent className="dialog_content">
-                <TextField
-                  margin="dense"
-                  id="name"
-                  label="Task name"
-                  type="text"
-                  fullWidth
-                  variant="outlined"
-                  helperText={
-                    errors.name && touched.name
-                      ? errors.name
-                      : null
-                  }
-                  error={
-                    errors.name && touched.name ? true : false
-                  }
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  required
-                />
-                <TextField
-                  id="description"
-                  margin="dense"
-                  label="Description"
-                  multiline
-                  fullWidth
-                  rows={3}
-                  helperText={
-                    errors.description && touched.description
-                      ? errors.description
-                      : null
-                  }
-                  error={
-                    errors.description && touched.description ? true : false
-                  }
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  required
-                />
-              </DialogContent>
-              <DialogActions className="button_group">
-                <Button
-                  onClick={handleClose}
-                  variant="outlined"
-                  sx={{ color: "#49B093", borderColor: "#49B093" }}
-                  className="buttons"
-                >
-                  Cancel
-                </Button>
-                <Button
-                  onClick={handleClose}
-                  variant="outlined"
-                  sx={{
-                    backgroundColor: "#49B093",
-                    borderColor: "#49B093",
-                    color: "white",
-                  }}
-                  className="buttons"
-                  type="submit"
-                >
-                  Add
-                </Button>
-              </DialogActions>
-            </Dialog>
-          </Form>
+          <ThemeProvider theme={theme}>
+            <Form>
+              <Button variant="contained" onClick={handleClickOpen}>
+                Add task
+              </Button>
+              <Dialog open={open} onClose={handleClose} fullWidth maxWidth="sm">
+                <DialogTitle className="dialog_title">
+                  Add a new task
+                </DialogTitle>
+                <DialogContent className="dialog_content">
+                  <TextField
+                    margin="dense"
+                    id="name"
+                    label="Task name"
+                    type="text"
+                    fullWidth
+                    variant="outlined"
+                    helperText={
+                      errors.name && touched.name ? errors.name : null
+                    }
+                    error={errors.name && touched.name ? true : false}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    required
+                  />
+                  <TextField
+                    id="description"
+                    margin="dense"
+                    label="Description"
+                    multiline
+                    fullWidth
+                    rows={3}
+                    helperText={
+                      errors.description && touched.description
+                        ? errors.description
+                        : null
+                    }
+                    error={
+                      errors.description && touched.description ? true : false
+                    }
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    required
+                  />
+                </DialogContent>
+                <DialogActions className="button_group">
+                  <Button
+                    onClick={handleClose}
+                    variant="outlined"
+                    className="buttons"
+                  >
+                    Cancel
+                  </Button>
+                  <Button
+                    onClick={handleClose}
+                    variant="outlined"
+                    className="buttons"
+                    type="submit"
+                  >
+                    Add
+                  </Button>
+                </DialogActions>
+              </Dialog>
+            </Form>
+          </ThemeProvider>
         );
       }}
     </Formik>
